993f2fef5ae0345452a4daf336c037b7e502cb35
lucasalves0722 <lucasalves098messias@gmail.com>
2024-05-24 19:33:34 -0300

    create ServicoMensagem class

---
+++ src/ServicoMensagemInstantanea.java
@@ -0,0 +1,22 @@
+public class ServicoMensagemInstantanea {
+    public class MSNMessenger {
+        public void enviarMensagem() {
+            validarConectadoInternet(); //encapsulado
+            System.out.println("Enviando mensagem");
+            salvarHitoricoMensagem(); //encapsulado
+        }
+    
+        public void receberMensagem() {
+            System.out.println("Recebendo mensagem");
+        }
+    
+        private void validarConectadoInternet() {
+            System.out.println("Validando se está conectado à internet");
+        }
+    
+        private void salvarHitoricoMensagem() {
+            System.out.println("Salvando o histórico da mensagem");
+        }
+    }
+    
+}
