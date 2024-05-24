2efbb5d785342a89af43b00f3d6517c449c14ae5
lucasalves0722 <lucasalves098messias@gmail.com>
2024-05-24 18:52:57 -0300

    Class MSN

---
+++ src/MSNMessenger.java
@@ -0,0 +1,17 @@
+public class MSNMessenger {
+    public void enviarMensagem() {
+        System.out.println("Enviar mensagem");
+    }
+
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    public void validarConectadoInternet() {
+        System.out.println("Validando se está conectado à internet");
+    }
+
+    public void salvarHitoricoMensagem() {
+        System.out.println("Salvando o histórico da mensagem");
+    }
+}
